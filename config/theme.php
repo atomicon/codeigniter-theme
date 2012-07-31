b4df32db02ea66f20b9274a5353134440c90f0d7
Atomicon <info@atomicon.nl>
2012-07-31 17:59:35 +0200

    Kristories pointed out that the path configuration didn't work out on linux-based systems (on PC & MAC it did)

--- config/theme.php
+++ config/theme.php
@@ -4,8 +4,8 @@
  *
  */
 
-//This is the physical path to the themes (Thanks Marcus Reinhardt, for the Mac fix)
-$config['theme']['path'] = FCPATH . 'themes/';
+//This is the physical path to the themes (Thanks Marcus Reinhardt & Kristories, for the Mac and Linux fix)
+$config['theme']['path'] = FCPATH . '/themes/';
 
 //This is the url to the themes path
 $config['theme']['url'] = trim(config_item('base_url'), '/ ') . '/themes/';
